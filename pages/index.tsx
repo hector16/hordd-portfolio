79efa0082af13c7db5d97688a2a7c7db15b14df0
hector16 <hrodriguez1717@gmail.com>
2021-08-12 11:28:21 -0600

    Add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ const Home = () => {
   return (
     <div className={styles.container}>
       <Layout>
+        <title>PixelOpedia</title>
         <Title> PixelOpedia </Title>
         <SubTitle>Coming soon...</SubTitle>
       </Layout>
